c97bd2a6b13959619fb4c5a6b494b072c19c502c
libertyernie <maybeway36@gmail.com>
2019-03-19 17:32:06 -0500

    Add example

---
+++ Example1/AudioWrapper.cs
@@ -0,0 +1,54 @@
+﻿using BrawlLib.LoopSelection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    public class AudioWrapper : IAudioStream
+    {
+        private readonly byte[] _pcmDataSource;
+
+        public AudioWrapper(byte[] pcmDataSource, int channels, int frequency)
+        {
+            _pcmDataSource = pcmDataSource;
+            Channels = channels;
+            Frequency = frequency;
+        }
+
+        public WaveFormatTag Format => WaveFormatTag.WAVE_FORMAT_PCM;
+
+        public int BitsPerSample => 16;
+
+        public int Samples => checked((int)(_pcmDataSource.Length / (2 * Channels)));
+
+        public int Channels { get; }
+
+        public int Frequency { get; }
+
+        public bool IsLooping { get; set; }
+        public int LoopStartSample { get; set; }
+        public int LoopEndSample { get; set; }
+
+        public int SamplePosition { get; set; }
+
+        public void Dispose() { }
+
+        public int ReadSamples(IntPtr destAddr, int numSamples)
+        {
+            int bytes_read = 2 * Channels * Math.Min(numSamples, Samples - SamplePosition);
+            Marshal.Copy(_pcmDataSource, 2 * Channels * SamplePosition, destAddr, bytes_read);
+            SamplePosition += bytes_read / (2 * Channels);
+            return bytes_read;
+        }
+
+        public void Wrap()
+        {
+            SamplePosition = LoopStartSample;
+        }
+    }
+}
---
+++ Example1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using BrawlLib.LoopSelection;
+using MP3Sharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                using (var stream = new MP3Stream(openFileDialog1.FileName))
+                using (var ms = new MemoryStream())
+                {
+                    await stream.CopyToAsync(ms);
+                    var wrapper = new AudioWrapper(ms.ToArray(), stream.ChannelCount, stream.Frequency);
+                    using (var dialog = new BrstmConverterDialog(wrapper))
+                    {
+                        if (dialog.ShowDialog(this) == DialogResult.OK)
+                        {
+                            propertyGrid1.SelectedObject = wrapper;
+                        }
+                    }
+                }
+            }
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (propertyGrid1.SelectedObject is AudioWrapper wrapper)
+            {
+                using (var dialog = new BrstmConverterDialog(wrapper))
+                {
+                    dialog.ShowDialog(this);
+                    propertyGrid1.SelectedObject = wrapper;
+                }
+            }
+        }
+    }
+}
